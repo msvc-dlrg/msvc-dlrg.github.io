61afdbdd10414a957bc3465a32f9687ff54ec7f3
geshengpsn <gesheng199832@126.com>
2024-12-12 19:49:02 +0800

    add rerun

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { WebViewer } from "@rerun-io/web-viewer";
+
+
+const rrd =
+  new URLSearchParams(location.search).get("url") ||
+  "https://app.rerun.io/version/0.20.3/examples/structure_from_motion.rrd";
+
+const viewer = new WebViewer();
+
+// get div id=rerun
+const rerun = document.getElementById("rerun"); 
+
+// console.log(rerun);
+
+viewer.start(rrd, rerun, {
+  width: "100%",
+  height: "100%",
+});
